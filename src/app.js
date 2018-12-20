fe3e5931656bfa0444076bd4e61cfc8a080afd0a
Ivan <ivanakazver3d@gmail.com>
2018-12-20 14:29:59 +0700

    Add react to the project

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1 style={{ color: 'red' }}>Hi, this is react!</h1>,
+    document.getElementById('react-root')
+);
--- src/main.js
+++ src/main.js
@@ -1,12 +1,6 @@
-require('babel-runtime/regenerator');
+require('@babel/runtime/regenerator');
+require('@babel/register');
 require('webpack-hot-middleware/client?reload=true');
 require('./main.css');
 require('./index.html');
-
-const a = async (args) => {
-    const { a, b } = args;
-    await console.log('Hi from arrow function');
-    console.log(a + b);
-}
-
-a({ a: 1, b: 2 });
+require('./app');
