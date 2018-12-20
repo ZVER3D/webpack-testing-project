fb5449b7fc5430b67219fad21f0dddc8fd4498b1
Ivan <ivanakazver3d@gmail.com>
2018-12-20 18:48:21 +0700

    Add react-hot-loader functionality, that prevents state to change upon hot reloading

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Counter from './counter';
+import { AppContainer } from 'react-hot-loader';
 
-ReactDOM.render(
-    <h1 style={{ color: 'red' }}>Hi, this is react!</h1>,
-    document.getElementById('react-root')
-);
+function render(Component) {
+    ReactDOM.render(
+        <AppContainer>
+            <Component />
+        </AppContainer>,
+        document.getElementById('react-root')
+    );
+}
+
+render(Counter);
+
+if (module.hot) {
+    module.hot.accept('./counter.js', () => {
+        const NewCounter = require('./counter').default;
+        render(NewCounter);
+    });
+}
